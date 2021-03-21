50f2d380b542956925669fbd7f512c47d91b629d
saiie5 <56484744+saiie5@users.noreply.github.com>
2021-03-21 11:43:56 +0530

    added some changes in code

--- Game2048-master/field.js
+++ Game2048-master/field.js
@@ -1,28 +1,28 @@
 function Field(canvas, n = 4) {
-    this.canvas = canvas // запоминаем картинку
-    this.n = n // запоминаем количество клеток на поле
+    this.canvas = canvas 
+    this.n = n 
     this.ctx = this.canvas.getContext("2d")
 
-    this.cellSize = 100 // размер клетки
-    this.size = this.cellSize * this.n // размер поля
-    this.paddingTop = 50 // отсутп сверху
-    this.paddingBottom = 10 // отступ снизу
-    this.paddingHor = 10 // отсупы по бокам
+    this.cellSize = 100 
+    this.size = this.cellSize * this.n 
+    this.paddingTop = 50 
+    this.paddingBottom = 10
+    this.paddingHor = 10
     
-    this.canvas.width = this.size + this.paddingHor * 2 // задаём ширину картинки
-    this.canvas.height = this.size + this.paddingTop + this.paddingBottom // задаём высоту картинки
+    this.canvas.width = this.size + this.paddingHor * 2 
+    this.canvas.height = this.size + this.paddingTop + this.paddingBottom 
 
-    this.fieldColor = "#bdaca0" // цвет поля
+    this.fieldColor = "#bdaca0" 
     this.colors = [ 
         "#cdc2b3", "#efe5da", "#ece0c8", "#f0b17d", "#f19867", "#f07e63",
         "#f46141", "#eacf78", "#edcd66", "#ecc75b", "#e8c256", "#e9be4c",
     ]
 
-    this.InitCells() // создаём клетки
-    this.AddCell(1) // добавляем двойку
-    this.AddCell(Math.random() < 0.75 ? 1 : 2) // добавляем 2 или 4
+    this.InitCells() 
+    this.AddCell(1) 
+    this.AddCell(Math.random() < 0.75 ? 1 : 2) 
 
-    this.score = 0 // очки
+    this.score = 0 
 
     let field = this
     document.addEventListener('keydown', function(e) {
@@ -30,7 +30,7 @@ function Field(canvas, n = 4) {
     })
 }
 
-// инициализация ячеек
+
 Field.prototype.InitCells = function() {
     this.cells = []
 
@@ -42,24 +42,24 @@ Field.prototype.InitCells = function() {
     }
 }
 
-// добавление новой клетки на поле
+
 Field.prototype.AddCell = function(value) {
     let availablePoints = [] // список пустых клеток
 
     for (let i = 0; i < this.n; i++)
         for (let j = 0; j < this.n; j++)
-            if (this.cells[i][j] == 0) // если клетка пустая
-                availablePoints.push({ i: i, j: j }) // добавляем её
+            if (this.cells[i][j] == 0)
+                availablePoints.push({ i: i, j: j })
 
     if (availablePoints.length == 0)
-        return false // нельзя добавить элемент
+        return false
 
     let point = availablePoints[Math.floor(Math.random() * availablePoints.length)]
-    this.cells[point.i][point.j] = value // добавляем ячейку
-    return true // добавление было
+    this.cells[point.i][point.j] = value
+    return true 
 }
 
-// отрисовка одной ячееки
+
 Field.prototype.DrawCell = function(i, j) {
     let value = this.cells[i][j]
     let x = this.paddingHor + j * this.cellSize
@@ -75,7 +75,7 @@ Field.prototype.DrawCell = function(i, j) {
     this.ctx.fillText(1 << value, x + this.cellSize / 2, y + this.cellSize / 2)
 }
 
-// отрисовка ячеек
+
 Field.prototype.DrawCells = function() {
     this.ctx.font = this.cellSize / 2.5 +"px Arial"
 
@@ -84,7 +84,7 @@ Field.prototype.DrawCells = function() {
             this.DrawCell(i, j)
 }
 
-// отрисовка поля
+
 Field.prototype.Draw = function() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
@@ -103,7 +103,6 @@ Field.prototype.Draw = function() {
     this.DrawCells()
 }
 
-// сдвиг значений в одну сторону
 Field.prototype.Shift = function(points) {
     let j = 0;
     let wasShift = true
@@ -111,7 +110,7 @@ Field.prototype.Shift = function(points) {
 
     for (let i = 0; i < points.length; i++) {
         if (this.cells[points[i].i][points[i].j] == 0)
-            continue // игнорируем пустые клетки
+            continue 
 
         this.cells[points[j].i][points[j].j] = this.cells[points[i].i][points[i].j]
 
@@ -134,7 +133,7 @@ Field.prototype.Shift = function(points) {
     return result
 }
 
-// проверка возможности сдвига значений в одну сторону
+
 Field.prototype.CanShift = function(points) {
     if (this.cells[points[0].i][points[0].j] == 0)
         return true
@@ -150,7 +149,6 @@ Field.prototype.CanShift = function(points) {
     return false
 }
 
-// проверка возможности сдвинуть куда-либо
 Field.prototype.IsGameOver = function() {
     for (let i = 0; i < this.n; i++) {
         let left = []
@@ -175,7 +173,7 @@ Field.prototype.IsGameOver = function() {
     return true
 }
 
-// сдвиг влево
+
 Field.prototype.ShiftLeft = function() {
     let result = false
 
@@ -191,7 +189,7 @@ Field.prototype.ShiftLeft = function() {
     return result
 }
 
-// сдвиг вправо
+
 Field.prototype.ShiftRight = function() {
     let result = false
 
@@ -207,7 +205,7 @@ Field.prototype.ShiftRight = function() {
     return result
 }
 
-// сдвиг вверх
+
 Field.prototype.ShiftUp = function() {
     let result = false
 
@@ -223,7 +221,6 @@ Field.prototype.ShiftUp = function() {
     return result
 }
 
-// сдвиг вниз
 Field.prototype.ShiftDown = function() {
     let result = false
 
@@ -239,7 +236,6 @@ Field.prototype.ShiftDown = function() {
     return result
 }
 
-// обработка нажатия клавиши
 Field.prototype.KeyDown = function(e) {
     let result = false
 
